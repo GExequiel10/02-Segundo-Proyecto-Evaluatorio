0a3b227193dfb498608d832f7e7bdf61ccbdc703
Exequiel Gonzalez <exe.gonzalez2010@gmail.com>
2024-11-24 11:40:12 -0300

    Agrega la tabla "alembic_version"  para el archivo de control. Crea una nueva base de datos

--- server/external_interface/products_api.py
+++ server/external_interface/products_api.py
@@ -6,13 +6,13 @@ from server.configs import app_settings
 
 logger = logging.getLogger(__name__)
 
+
 class ProductsApi:
     def __init__(self) -> None:
         self.client = requests.Session()
         self.base_url = app_settings.PRODUCTS_API
 
-        
-    def get_list(self, limit:int, offset:int):
+    def get_list(self, limit: int, offset: int):
         url = self.base_url + '/products'
         params = {
             'limit': limit,
@@ -20,5 +20,5 @@ class ProductsApi:
         }
         response = self.client.get(url, params=params)
         logger.info(f'[GET]{response.url}:{response.status_code}')
-       
+
         return response.json()
